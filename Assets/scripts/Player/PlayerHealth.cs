29dd0b6a84ac4f95f064c046c561f5ece308c856
Rudar12222 <founder.nava@gmail.com>
2025-11-17 13:03:39 +0530

    playerr health bar

--- Assets/scripts/Player/PlayerHealth.cs
+++ Assets/scripts/Player/PlayerHealth.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Player Health Settings")]
     public int maxHealth = 100;
     public int currentHealth;
+    public TMP_Text healthtext;
 
     private void Start()
     {
         currentHealth = maxHealth; // Initialize health at start
+        healthtext.text ="HP:"+ currentHealth + "/" + maxHealth;
+    
     }
 
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
-
+         healthtext.text ="HP:"+ currentHealth + "/" + maxHealth;
         // Clamp between 0 and maxHealth
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
